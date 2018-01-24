86381ed461e694bf53accc9e5efa330d56101a5f
Takuya Fukuju <chalkygames123@icloud.com>
2018-01-24 16:27:14 +0900

    develop コマンドを修正。

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,14 +1,10 @@
 const gulp = require('gulp')
-const config = require('../config')
 const runSequence = require('run-sequence')
-
-const defaultTasks = ['copy', 'html', 'images', 'styles', 'scripts']
-const watchTasks = ['copy', 'html', 'images', 'styles']
 
 const build = done => {
 	runSequence(
 		'clean',
-		config.watch ? watchTasks : defaultTasks,
+		['copy', 'html', 'images', 'styles', 'scripts'],
 		done
 	)
 }
