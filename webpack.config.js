c0d04cff7d7f775879caaedc22bc9edf4158ef2b
Takuya Fukuju <chalkygames123@icloud.com>
2018-02-16 16:37:01 +0900

    webpack.config.js: リファクタリング。

--- webpack.config.js
+++ webpack.config.js
@@ -49,8 +49,8 @@ module.exports = {
   },
   resolve: {
     modules: [
-      'src/assets/scripts',
-      'node_modules'
+      'node_modules',
+      'src/assets/scripts'
     ]
   },
   plugins: commonPlugins.concat(PRODUCTION ? productionPlugins : developmentPlugins),
