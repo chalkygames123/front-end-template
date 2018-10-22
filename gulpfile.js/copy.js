887cbb144318b4f16ad3cbee05e0e935b067df5b
Takuya Fukuju <chalkygames123@icloud.com>
2018-10-22 13:21:44 +0900

    静的ファイルのコピー先が既に存在する場合エラーとなるよう変更
    - 開発中には gulp-plumber と gulp-notify が適用されるよう変更

--- gulpfile.js/copy.js
+++ gulpfile.js/copy.js
@@ -1,12 +1,16 @@
 const fs = require('fs')
 const path = require('path')
 
+const $ = require('gulp-load-plugins')()
 const chalk = require('chalk')
 const gulp = require('gulp')
+const stripAnsi = require('strip-ansi')
 const through2 = require('through2')
 const upath = require('upath')
 
 const config = require(path.resolve('config'))
+
+const isDev = config.env === 'development'
 
 function copy () {
   return gulp
@@ -16,20 +20,25 @@ function copy () {
       dot: true,
       nodir: true
     })
+    .pipe($.if(isDev, $.plumber({
+      errorHandler: $.notify.onError()
+    })))
     .pipe(through2.obj((file, encoding, cb) => {
       const destPath = upath.join(config.distDir, file.relative)
 
       fs.access(destPath, fs.constants.F_OK, error => {
         if (!error) {
-          console.warn(chalk.yellow(
-            `Warning: ${
+          const errorMessage = chalk.red(
+            `${
               chalk.underline(upath.join(file.base, file.relative))
             } conflicts with ${
               chalk.underline(destPath)
             }`
-          ))
+          )
 
-          return cb()
+          console.error(errorMessage)
+
+          return cb(new Error(stripAnsi(errorMessage)))
         }
 
         return cb(null, file)
