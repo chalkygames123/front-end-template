839dcfdba5a440a258a367675ed4a9773e8e9af1
Takuya Fukuju <chalkygames123@icloud.com>
2018-11-08 20:20:29 +0900

    build タスクの余分な依存タスクを削除
    styles タスクが images タスクに依存しているため
    build タスクの images タスクは不要だった。

--- gulpfile.js/build.js
+++ gulpfile.js/build.js
@@ -3,11 +3,10 @@ const gulp = require('gulp')
 const clean = require('./clean')
 const copy = require('./copy')
 const html = require('./html')
-const images = require('./images')
 const scripts = require('./scripts')
 const styles = require('./styles')
 
 module.exports = gulp.series(
   clean,
-  gulp.parallel(copy, html, images, scripts, styles)
+  gulp.parallel(copy, html, scripts, styles)
 )
