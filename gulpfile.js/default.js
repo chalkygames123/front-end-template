dc85079939e13ac4e987dc0914ad842ef2dd780a
Takuya Fukuju <chalkygames123@icloud.com>
2018-11-08 12:43:03 +0900

    リファクタリング

--- gulpfile.js/default.js
+++ gulpfile.js/default.js
@@ -1,5 +1,3 @@
-const gulp = require('gulp')
-
 const serve = require('./serve')
 
-module.exports = gulp.series(serve)
+module.exports = serve
--- gulpfile.js/watch.js
+++ gulpfile.js/watch.js
@@ -10,11 +10,11 @@ const scripts = require('./scripts')
 const styles = require('./styles')
 
 function watch () {
-  gulp.watch(common.srcPaths.copy, gulp.series(copy))
-  gulp.watch(common.srcPaths.html, gulp.series(html))
-  gulp.watch(common.srcPaths.images, gulp.series(images))
-  gulp.watch(common.srcPaths.scripts, gulp.series(scripts))
-  gulp.watch(common.srcPaths.styles, gulp.series(styles))
+  gulp.watch(common.srcPaths.copy, copy)
+  gulp.watch(common.srcPaths.html, html)
+  gulp.watch(common.srcPaths.images, images)
+  gulp.watch(common.srcPaths.scripts, scripts)
+  gulp.watch(common.srcPaths.styles, styles)
 }
 
 module.exports = watch
