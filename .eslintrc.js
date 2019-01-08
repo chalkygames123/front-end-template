c82b06228d65c5c30a468d7d21f340c98def158a
Takuya Fukuju <chalkygames123@icloud.com>
2019-01-09 01:37:46 +0900

    .eslintrc.js をフォーマット

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,6 @@
 module.exports = {
   root: true,
-  extends: [
-    'standard',
-    'plugin:prettier/recommended',
-    'prettier/standard'
-  ],
+  extends: ['standard', 'plugin:prettier/recommended', 'prettier/standard'],
   env: {
     browser: true
   }
