86e224fedba294bd4975bbdae663415d1be7828c
Takuya Fukuju <chalkygames123@icloud.com>
2019-03-31 21:35:54 +0900

    normalize.css を postcss-normalize に置き換え

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,10 @@
 import autoprefixer from 'autoprefixer'
 import cssMqpacker from 'css-mqpacker'
+import postcssNormalize from 'postcss-normalize'
 
 export default {
   plugins: [
+    postcssNormalize(),
     autoprefixer({
       grid: 'autoplace'
     }),
