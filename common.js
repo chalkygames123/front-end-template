61146166cd788123cb79cb3dbc4a35a1e8a8ecbb
Takuya Fukuju <chalkygames123@icloud.com>
2019-04-22 15:23:58 +0900

    common.js をリファクタリング

--- common.js
+++ common.js
@@ -21,27 +21,28 @@ const common = {
     sprites: '.svg',
     styles: '.scss'
   },
-  server: browserSync.create()
-}
-
-common.srcPaths = {
-  copy: [`${config.get('srcDir')}/${common.dir.static}/**`, '!**/.gitkeep'],
-  images: `${config.get('srcDir')}/${common.dir.assets}/${
-    common.dir.images
-  }/**/*${common.ext.images}`,
-  includes: `${config.get('srcDir')}/${common.dir.includes}/**/*${
-    common.ext.pages
-  }`,
-  layouts: `${config.get('srcDir')}/${common.dir.layouts}/**/*${
-    common.ext.pages
-  }`,
-  pages: `${config.get('srcDir')}/${common.dir.pages}/**/*${common.ext.pages}`,
-  scripts: `${config.get('srcDir')}/${common.dir.assets}/${
-    common.dir.scripts
-  }/**/*${common.ext.scripts}`,
-  styles: `${config.get('srcDir')}/${common.dir.assets}/${
-    common.dir.styles
-  }/**/*${common.ext.styles}`
+  server: browserSync.create(),
+  get srcPaths() {
+    return {
+      copy: [`${config.get('srcDir')}/${this.dir.static}/**`, '!**/.gitkeep'],
+      images: `${config.get('srcDir')}/${this.dir.assets}/${
+        this.dir.images
+      }/**/*${this.ext.images}`,
+      includes: `${config.get('srcDir')}/${this.dir.includes}/**/*${
+        this.ext.pages
+      }`,
+      layouts: `${config.get('srcDir')}/${this.dir.layouts}/**/*${
+        this.ext.pages
+      }`,
+      pages: `${config.get('srcDir')}/${this.dir.pages}/**/*${this.ext.pages}`,
+      scripts: `${config.get('srcDir')}/${this.dir.assets}/${
+        this.dir.scripts
+      }/**/*${this.ext.scripts}`,
+      styles: `${config.get('srcDir')}/${this.dir.assets}/${
+        this.dir.styles
+      }/**/*${this.ext.styles}`
+    }
+  }
 }
 
 export default common
