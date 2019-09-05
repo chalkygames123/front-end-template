179cb1474a6ce2e5a269391cbb588670482afeea
Takuya Fukuju <chalkygames123@icloud.com>
2019-09-05 22:58:55 +0900

    styles.js を修正

--- gulpfile.js/tasks/styles.js
+++ gulpfile.js/tasks/styles.js
@@ -50,7 +50,7 @@ export default function styles() {
           dependentPaths.push(file.path)
 
           graph.visitAncestors(file.path, path => {
-            if (dependentPaths.includes(path) < 0) {
+            if (dependentPaths.includes(path) === false) {
               dependentPaths.push(path)
             }
           })
