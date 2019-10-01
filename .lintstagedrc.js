11c696240f3a05d6d7ea545788dacfbd9117ff5d
Takuya Fukuju <chalkygames123@icloud.com>
2019-10-01 18:10:26 +0900

    lint かフォーマットの設定が変更されたときにテストを実行するよう変更

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,6 @@
 module.exports = {
   '*.js': ['eslint --ignore-pattern "!.*" --fix', 'git add'],
   '*.scss': ['stylelint --fix', 'git add'],
-  '*.{json,md}': ['prettier --check --write', 'git add']
+  '*.{json,md}': ['prettier --check --write', 'git add'],
+  '{.eslintrc.js,.stylelintrc.js,.prettierrc.js}': () => 'npm-run-all test'
 }
