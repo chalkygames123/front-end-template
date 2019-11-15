2f75fddfb4613082d9edb2a4703ac1544e36ccb7
Takuya Fukuju <chalkygames123@icloud.com>
2019-11-16 00:33:33 +0900

    lint-staged の .scss 用の設定を修正
    `stylelint --fix` が期待通りに 2 回動いていなかった。

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   '*.js': ['eslint --ignore-pattern "!.*" --fix', 'git add'],
-  '*.scss': ['stylelint --fix; stylelint --fix', 'git add'],
+  '*.scss': ['stylelint --fix', 'stylelint --fix', 'git add'],
   '*.{json,md}': ['prettier --check --write', 'git add'],
   '{.eslintrc.js,.stylelintrc.js,.prettierrc.js}': () => 'npm-run-all test'
 }
