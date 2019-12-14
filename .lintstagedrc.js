ff27193b4ed23d93e8ca54f5c57836b73eb7a212
Takuya Fukuju <chalkygames123@icloud.com>
2019-12-14 20:55:00 +0900

    .lintstagedrc.js を更新

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,8 +1,5 @@
 module.exports = {
   '*.js': ['eslint --ignore-pattern "!.*" --fix', 'git add'],
   '*.scss': ['stylelint --fix', 'stylelint --fix', 'git add'],
-  '*.{json,md}': ['prettier --check --write', 'git add'],
-  '.eslintrc.js': () => 'npm-run-all lint:scripts',
-  '.stylelintrc.js': () => 'npm-run-all lint:styles',
-  '.prettierrc.js': () => 'npm-run-all lint check:format'
+  '*.{json,md}': ['prettier --check --write', 'git add']
 }
