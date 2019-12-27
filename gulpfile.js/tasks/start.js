9a7f9d02c5789387630ae3645d8d0b51fb31c50d
Takuya Fukuju <chalkygames123@icloud.com>
2019-12-27 22:57:32 +0900

    gulp の設定をリファクタリング

--- gulpfile.js/tasks/start.js
+++ gulpfile.js/tasks/start.js
@@ -1,11 +1,7 @@
 import gulp from 'gulp'
 
-import config from '../../config'
-
 import build from './build'
 import serve from './serve'
 import watch from './watch'
 
-const isDev = config.get('mode') !== 'production'
-
-export default gulp.series(build, isDev ? gulp.parallel(serve, watch) : serve)
+export default gulp.series(build, gulp.parallel(serve, watch))
