355f8880a1dc90157fc9424e6fefa30c0313b104
Takuya Fukuju <chalkygames123@gmail.com>
2020-02-13 19:32:07 +0900

    @prettier/plugin-pug を追加

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   '*.js': 'eslint --ignore-pattern "!.*" --fix',
   '*.scss': ['stylelint --fix', 'stylelint --fix'],
-  '*.{json,md,y?(a)ml}': 'prettier --check --write'
+  '*.{json,md,pug,y?(a)ml}': 'prettier --check --write'
 }
