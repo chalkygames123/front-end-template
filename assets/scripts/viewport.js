58b80c776483010f4a94bca72dc7f24ac3ffd62b
Takuya Fukuju <chalkygames123@gmail.com>
2020-06-23 11:39:23 +0900

    viewport.js の実行タイミングを修正

--- assets/scripts/viewport.js
+++ assets/scripts/viewport.js
@@ -1,14 +1,12 @@
-const minWidth = 375
-const viewportEl = document.querySelector('meta[name="viewport"]')
-const mediaQueryList = window.matchMedia(`(min-device-width: ${minWidth}px)`)
-
-function handleChange() {
-  const viewportContent = mediaQueryList.matches
-    ? 'width=device-width, initial-scale=1'
-    : `width=${minWidth}`
-
-  viewportEl.setAttribute('content', viewportContent)
+const el = document.querySelector('meta[name="viewport"]')
+const mql = window.matchMedia('(min-device-width: 375px)')
+const handleChange = () => {
+  el.setAttribute(
+    'content',
+    mql.matches ? 'width=device-width, initial-scale=1' : 'width=375'
+  )
 }
 
-mediaQueryList.addListener(handleChange)
 handleChange()
+
+mql.addListener(handleChange)
