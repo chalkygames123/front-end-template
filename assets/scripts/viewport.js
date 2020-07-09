e67da51e770bacd721247030522e97284163a695
Takuya FUKUJU <chalkygames123@users.noreply.github.com>
2020-07-09 14:24:33 +0900

    Update viewport.js

--- assets/scripts/viewport.js
+++ assets/scripts/viewport.js
@@ -1,12 +1,14 @@
 const el = document.querySelector('meta[name="viewport"]')
 const mql = window.matchMedia('(min-device-width: 375px)')
-const handleChange = () => {
-  el.setAttribute(
-    'content',
-    mql.matches ? 'width=device-width, initial-scale=1' : 'width=375'
-  )
+
+const updateContent = (matches) => {
+  el.setAttribute('content', matches ? 'width=device-width' : 'width=375')
 }
 
-handleChange()
+updateContent(mql.matches)
+
+const handleChange = (e) => {
+  updateContent(e.matches)
+}
 
 mql.addListener(handleChange)
