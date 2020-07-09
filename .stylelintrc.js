bcfc8f326a68b568eebb8c28cdcb8e2a594446ab
Takuya FUKUJU <chalkygames123@users.noreply.github.com>
2020-07-09 20:29:45 +0900

    Update .stylelintrc.js

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -56,6 +56,10 @@ module.exports = {
           type: 'rule',
           selector: /^&:\w/,
         },
+        {
+          type: 'rule',
+          selector: /^&/,
+        },
       ],
     ],
     'rule-empty-line-before': [
