6446101231789d65b5975ce832c187cd2d6d74ae
Takuya FUKUJU <chalkygames123@users.noreply.github.com>
2020-07-09 17:16:23 +0900

    Update viewport.js

--- assets/scripts/viewport.js
+++ assets/scripts/viewport.js
@@ -1,14 +1,10 @@
 const el = document.querySelector('meta[name="viewport"]')
 const mql = window.matchMedia('(min-device-width: 375px)')
 
-const updateContent = (matches) => {
+const updateContent = ({ matches }) => {
   el.setAttribute('content', matches ? 'width=device-width' : 'width=375')
 }
 
-updateContent(mql.matches)
+updateContent(mql)
 
-const handleChange = (e) => {
-  updateContent(e.matches)
-}
-
-mql.addListener(handleChange)
+mql.addListener(updateContent)
