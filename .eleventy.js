e15dcd904a2f61fdc14c08ebb6fe8cd4e23a0f4f
Takuya Fukuju <chalkygames123@gmail.com>
2020-07-29 00:09:16 +0900

    .eleventy.js をリファクタリング

--- .eleventy.js
+++ .eleventy.js
@@ -13,47 +13,45 @@ const htmlhintRules = JSON.parse(fs.readFileSync('.htmlhintrc', 'utf8'))
 
 module.exports = (eleventyConfig) => {
   eleventyConfig.addTransform('html-minifier', (content, outputPath) => {
-    if (/\.(html|php)$/.test(outputPath) && !isDev) {
-      const result = htmlMinifier.minify(content, {
-        collapseBooleanAttributes: true,
-        collapseWhitespace: true,
-        decodeEntities: true,
-        minifyCSS: true,
-        minifyJS: true,
-        processConditionalComments: true,
-        removeComments: true,
-        removeEmptyAttributes: true,
-        removeRedundantAttributes: true,
-        removeScriptTypeAttributes: true,
-        removeStyleLinkTypeAttributes: true,
-        trimCustomFragments: true,
-        useShortDoctype: true,
-      })
+    if (!/\.(html|php)$/.test(outputPath) || isDev) return content
 
-      return result
-    }
+    const result = htmlMinifier.minify(content, {
+      collapseBooleanAttributes: true,
+      collapseWhitespace: true,
+      decodeEntities: true,
+      minifyCSS: true,
+      minifyJS: true,
+      processConditionalComments: true,
+      removeComments: true,
+      removeEmptyAttributes: true,
+      removeRedundantAttributes: true,
+      removeScriptTypeAttributes: true,
+      removeStyleLinkTypeAttributes: true,
+      trimCustomFragments: true,
+      useShortDoctype: true,
+    })
 
-    return content
+    return result
   })
 
   eleventyConfig.addLinter('htmlhint', (content, inputPath, outputPath) => {
-    if (outputPath.endsWith('.html')) {
-      const result = htmlhint.verify(content, htmlhintRules)
+    if (!outputPath.endsWith('.html')) return
 
-      if (result.length > 0) {
-        const report = htmlhint
-          .format(result, {
-            colors: true,
-            indent: 4,
-          })
-          .reduce((acc, line) => `${acc}\n${line}`, '')
+    const result = htmlhint.verify(content, htmlhintRules)
 
-        // eslint-disable-next-line no-console
-        console.error(
-          `HTMLHint: ${result.length} error(s) found in ${outputPath}${report}\n`
-        )
-      }
-    }
+    if (!result.length) return
+
+    const report = htmlhint
+      .format(result, {
+        colors: true,
+        indent: 4,
+      })
+      .reduce((acc, line) => `${acc}\n${line}`, '')
+
+    // eslint-disable-next-line no-console
+    console.error(
+      `HTMLHint: ${result.length} error(s) found in ${outputPath}${report}\n`
+    )
   })
 
   return {
