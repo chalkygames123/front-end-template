0bc66586b1ef4a7e2ee86057bb6e90f8d99e1e9f
Takuya Fukuju <chalkygames123@gmail.com>
2020-07-28 23:51:03 +0900

    default タスクを削除

--- gulpfile.js/tasks/index.js
+++ gulpfile.js/tasks/index.js
@@ -1,7 +1,6 @@
 module.exports.build = require('./build')
 module.exports.clean = require('./clean')
 module.exports.copy = require('./copy')
-module.exports.default = require('./start')
 module.exports.images = require('./images')
 module.exports.scripts = require('./scripts')
 module.exports.serve = require('./serve')
