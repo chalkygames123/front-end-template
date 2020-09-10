6161e47c06cbe43527f7cb31ce7b8cf7b6f92ccb
Takuya Fukuju <chalkygames123@gmail.com>
2020-09-11 00:46:45 +0900

    pre-commit フックによるフォーマットの対象を拡大
    Prettier がサポートするすべてのファイルタイプが対象となるように。

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,18 @@
+/* eslint 'import/no-extraneous-dependencies': ['error', { optionalDependencies: false }] */
+
+const prettier = require('prettier')
+
+const extensions = prettier
+  .getSupportInfo()
+  .languages.flatMap((info) => info.extensions)
+const extensionsToExclude = ['.js', '.scss']
+const pattern = `*.{${extensions
+  .filter((ext) => !extensionsToExclude.includes(ext))
+  .map((ext) => ext.slice(1))
+  .join(',')}}`
+
 module.exports = {
   '*.js': 'eslint --fix',
   '*.scss': ['stylelint --fix', 'stylelint --fix'],
-  '*.{json,md,y?(a)ml}': 'prettier --check --write',
+  [pattern]: 'prettier --check --write',
 }
