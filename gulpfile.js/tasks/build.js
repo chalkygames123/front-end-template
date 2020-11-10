aa134fc8f26306282786586735f30e4154e6d14a
Takuya Fukuju <chalkygames123@gmail.com>
2020-11-10 20:06:47 +0900

    依存関係削減のため sprites タスクを削除

--- gulpfile.js/tasks/build.js
+++ gulpfile.js/tasks/build.js
@@ -4,11 +4,10 @@ const clean = require('./clean')
 const copy = require('./copy')
 const images = require('./images')
 const scripts = require('./scripts')
-const sprites = require('./sprites')
 const styles = require('./styles')
 const templates = require('./templates')
 
 module.exports = series(
   clean,
-  parallel(copy, images, scripts, sprites, styles, templates)
+  parallel(copy, images, scripts, styles, templates)
 )
