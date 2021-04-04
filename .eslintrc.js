5f371e281e619f9ce8e0a374276dfff160e97e3c
Takuya Fukuju <chalkygames123@gmail.com>
2021-04-04 20:13:59 +0900

    .eslintrc.js#env.es2021 を true に設定

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   root: true,
   env: {
     browser: true,
+    es2021: true,
   },
   reportUnusedDisableDirectives: true,
   extends: ['airbnb-base', 'plugin:eslint-comments/recommended', 'prettier'],
