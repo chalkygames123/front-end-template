2bcefb881d89961a78f9c8f64cb88c9fb62726bb
Takuya Fukuju <chalkygames123@gmail.com>
2021-09-05 17:03:54 +0900

    `.eleventy.js`: `addTransform` と `addLinter` の順序を入れ替え
    実行される順序に一致させるため。

--- .eleventy.js
+++ .eleventy.js
@@ -12,6 +12,26 @@ const isDev = config.get('mode') !== 'production';
 const htmlhintRules = JSON.parse(fs.readFileSync('.htmlhintrc', 'utf8'));
 
 module.exports = (eleventyConfig) => {
+	eleventyConfig.addLinter('htmlhint', (content, inputPath, outputPath) => {
+		if (!outputPath.endsWith('.html')) return;
+
+		const result = htmlhint.verify(content, htmlhintRules);
+
+		if (!result.length) return;
+
+		const report = htmlhint
+			.format(result, {
+				colors: true,
+				indent: 4,
+			})
+			.reduce((acc, line) => `${acc}\n${line}`, '');
+
+		// eslint-disable-next-line no-console
+		console.error(
+			`HTMLHint: ${result.length} error(s) found in ${outputPath}${report}\n`
+		);
+	});
+
 	eleventyConfig.addTransform('html-minifier', (content, outputPath) => {
 		if (!/\.(?:html|php)$/.test(outputPath) || isDev) return content;
 
@@ -32,26 +52,6 @@ module.exports = (eleventyConfig) => {
 		});
 
 		return result;
-	});
-
-	eleventyConfig.addLinter('htmlhint', (content, inputPath, outputPath) => {
-		if (!outputPath.endsWith('.html')) return;
-
-		const result = htmlhint.verify(content, htmlhintRules);
-
-		if (!result.length) return;
-
-		const report = htmlhint
-			.format(result, {
-				colors: true,
-				indent: 4,
-			})
-			.reduce((acc, line) => `${acc}\n${line}`, '');
-
-		// eslint-disable-next-line no-console
-		console.error(
-			`HTMLHint: ${result.length} error(s) found in ${outputPath}${report}\n`
-		);
 	});
 
 	return {
