a5f67f57018a5599b8b0c8b55afcd2d8856c77a5
Takuya Fukuju <chalkygames123@gmail.com>
2021-09-29 19:57:54 +0900

    `clean.js`: 先祖ディレクトリの保護を追加

--- gulpfile.js/tasks/clean.js
+++ gulpfile.js/tasks/clean.js
@@ -1,8 +1,15 @@
 const { rm } = require('fs/promises');
+const { dirname, resolve } = require('path');
 
 const config = require('../../config');
 
-module.exports = function clean() {
+module.exports = function clean(cb) {
+	if (
+		dirname(process.cwd()).startsWith(dirname(resolve(config.get('distDir'))))
+	) {
+		cb(new Error('distDir cannot be a parent of or same as process.cwd()'));
+	}
+
 	return rm(config.get('distDir'), {
 		force: true,
 		recursive: true,
