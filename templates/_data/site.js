af6cf3dd6d180fb40b954785ccb8be82f2528af1
Takuya Fukuju <chalkygames123@gmail.com>
2022-04-19 17:17:08 +0900

    `base.njk`: generator メタタグに `eleventy.generator` グローバルデータを使用

--- templates/_data/site.js
+++ templates/_data/site.js
@@ -1,9 +1,4 @@
-/* eslint 'import/no-extraneous-dependencies': ['error', { optionalDependencies: false }] */
-
-const eleventyPkg = require('@11ty/eleventy/package.json');
-
 module.exports = {
 	name: 'Site Name',
 	origin: 'https://example.com',
-	eleventyVersion: eleventyPkg.version,
 };
