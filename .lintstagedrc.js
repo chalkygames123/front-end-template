5bc9603f222ed1269dec8496e30501c5f0de9a6a
Takuya Fukuju <chalkygames123@gmail.com>
2022-06-03 21:02:08 +0900

    `.lintstagedrc.js` をリファクタ
    参照: https://github.com/okonet/lint-staged/pull/997

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,18 +1,5 @@
-/* eslint 'import/no-extraneous-dependencies': ['error', { optionalDependencies: false }] */
-
-const { getSupportInfo } = require('prettier');
-
-const extensions = getSupportInfo().languages.flatMap(
-	(info) => info.extensions,
-);
-const extensionsToExclude = ['.js', '.scss'];
-const pattern = `*.{${extensions
-	.filter((ext) => !extensionsToExclude.includes(ext))
-	.map((ext) => ext.replace('.', ''))
-	.join()}}`;
-
 module.exports = {
 	'*.js': ['prettier --check --write', 'eslint --fix'],
 	'*.scss': ['prettier --check --write', 'stylelint --fix', 'stylelint --fix'],
-	[pattern]: 'prettier --check --write',
+	'!*.js|*.scss': 'prettier --check --write --ignore-unknown',
 };
