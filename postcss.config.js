2065f44ab2e39ff8ad876d5a4f7c57f68b70048d
Takuya Fukuju <chalkygames123@gmail.com>
2023-01-13 19:17:36 +0900

    feat(postcss)!: 出力に `publicPath` を自動的に適用する

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,18 @@
+const { isAbsolute, join } = require('node:path');
+
+const config = require('./config');
+
 module.exports = {
 	plugins: {
 		autoprefixer: {},
+		'postcss-url': {
+			url: (asset) => {
+				if (isAbsolute(asset.url)) {
+					return join(config.get('publicPath'), asset.url);
+				}
+
+				return asset.url;
+			},
+		},
 	},
 };
