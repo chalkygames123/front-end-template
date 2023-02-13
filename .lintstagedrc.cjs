d7865c072d659dd54ea8528aad408f57512a4f27
Takuya Fukuju <chalkygames123@gmail.com>
2023-02-13 18:06:05 +0900

    chore(lint-staged): `package.json` が変更されたとき `package-lock.json` を更新する
    関連: https://github.com/okonet/lint-staged/issues/1014

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -2,4 +2,5 @@ module.exports = {
 	'*.{js,mjs,cjs,jsx}': ['eslint --fix', 'prettier --check --write'],
 	'*.scss': ['stylelint --fix', 'stylelint --fix', 'prettier --check --write'],
 	'!*.{js,mjs,cjs,jsx}|*.scss': 'prettier --check --write --ignore-unknown',
+	'package.json': () => ['npm install', 'git add package-lock.json'],
 };
