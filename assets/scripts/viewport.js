7154940f3b6940f34fb55f072bb050049b2e63ce
Takuya Fukuju <chalkygames123@gmail.com>
2023-02-16 02:09:03 +0900

    fix: `viewport.js` で `element` が `null` である可能性を考慮する

--- assets/scripts/viewport.js
+++ assets/scripts/viewport.js
@@ -1,13 +1,16 @@
 const minWidth = 375;
 const element = document.querySelector('meta[name="viewport"]');
-const updateContent = () => {
-	if (window.screen.width < minWidth) {
-		element.setAttribute('content', `width=${minWidth}`);
-	} else {
-		element.setAttribute('content', 'width=device-width');
-	}
-};
 
-updateContent();
+if (element) {
+	const updateContent = () => {
+		if (window.screen.width < minWidth) {
+			element.setAttribute('content', `width=${minWidth}`);
+		} else {
+			element.setAttribute('content', 'width=device-width');
+		}
+	};
 
-window.addEventListener('orientationchange', updateContent);
+	updateContent();
+
+	window.addEventListener('orientationchange', updateContent);
+}
