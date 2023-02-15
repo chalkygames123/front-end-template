55cb917f1c49987edc9a1f1e187bfb8ddc55603b
Takuya Fukuju <chalkygames123@gmail.com>
2023-02-16 02:12:55 +0900

    feat(eslint): `@chalkygames123/eslint-config@6.0.0-beta.9` に移行する

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,18 +6,4 @@
 module.exports = {
 	root: true,
 	extends: ['@chalkygames123', 'prettier'],
-	overrides: [
-		{
-			files: '*',
-			excludedFiles: ['assets/scripts/**', 'modules/**'],
-			rules: {
-				'import/no-extraneous-dependencies': [
-					'error',
-					{
-						optionalDependencies: false,
-					},
-				],
-			},
-		},
-	],
 };
