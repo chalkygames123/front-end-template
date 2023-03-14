4ed17239a4b11ab1b6139ea1ab1c9e68c21a85f6
Takuya Fukuju <chalkygames123@gmail.com>
2023-03-14 19:23:42 +0900

    chore(lint-staged): `package.json` のためのコマンドを修正する

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,5 +2,8 @@ export default {
 	'*.{js,mjs,cjs,jsx}': ['eslint --fix', 'prettier --check --write'],
 	'*.scss': ['stylelint --fix', 'stylelint --fix', 'prettier --check --write'],
 	'!*.{js,mjs,cjs,jsx}|*.scss': 'prettier --check --write --ignore-unknown',
-	'package.json': () => ['npm install', 'git add package-lock.json'],
+	'package.json': () => [
+		'npm install --package-lock-only',
+		'git add package-lock.json',
+	],
 };
